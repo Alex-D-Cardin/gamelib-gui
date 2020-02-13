bc3bc1e73ce64882c658afa979fe1f814c390e15
Alex Cardin <acardin@debian.CIS.COM>
2020-02-13 13:27:01 -0600

    modified:   gamelib_gui.py

--- gamelib_gui.py
+++ gamelib_gui.py
@@ -15,6 +15,9 @@ BUTTON_FONT = ("Arial", 15)
 class MainMenu(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
+        
+        
+        
         self.lbl_title = tk.Label(text = "Game Library", font = TITLE_FONT)
         self.lbl_title.grid(row = 0, column = 0, sticky = "news")
         self.btn_add = tk.Button(text = "Add", font = BUTTON_FONT)
@@ -26,34 +29,41 @@ class MainMenu(tk.Frame):
         self.btn_remove = tk.Button(text = "Remove", font = BUTTON_FONT)
         self.btn_remove.grid(row = 4, column = 0)
         self.btn_save = tk.Button(text = "Save", font = BUTTON_FONT)
-        self.btn_save.grid(row = 5, column = 0)        
-
-
+        self.btn_save.grid(row = 5, column = 0)
+        
 class Search_Menu(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
         
-        self.lbl_title = tk.Label(text = "Search Menu", font = TITLE_FONT)
+        options = ['Genre', 'Title', 'Year', 'Developer', 'Publisher', 'Price', 'Gamemode', 'Console']
+        
+        self.lbl_title = tk.Label(self, text = "Search Menu", font = TITLE_FONT)
         self.lbl_title.grid(row = 0, column = 0, sticky = "news")
-        self.btn_back = tk.Button(text = "Back", font = BUTTON_FONT)
+        self.btn_back = tk.Button(self, text = "Back", font = BUTTON_FONT)
         self.btn_back.grid(row = 5, column = 0)
-        self.btn_clear = tk.Button(text = "Clear", font = BUTTON_FONT)
+        self.btn_clear = tk.Button(self, text = "Clear", font = BUTTON_FONT)
         self.btn_clear.grid(row = 5, column = 1)
-        self.btn_submit = tk.Button(text = "Submit", font = BUTTON_FONT)
+        self.btn_submit = tk.Button(self, text = "Submit", font = BUTTON_FONT)
         self.btn_submit.grid(row = 5, column = 2)
         
-        self.lbl_search_by = tk.Label(text = "Search By:", font = TITLE_FONT)
+        self.grid_rowconfigure(5, weight = 1)
+        
+        self.lbl_search_by = tk.Label(self, text = "Search By:", font = TITLE_FONT)
         self.lbl_search_by.grid(row = 1, column = 0)
-        self.ent_by = tk.Entry(font = BUTTON_FONT)
-        self.ent_by.grid(row = 2, column = 0)
-        self.lbl_search_for = tk.Label(text = "Search For:", font = TITLE_FONT)
+        self.tkvar = tk.StringVar(self)
+        self.tkvar.set(options)
+        self.dbx_search_by = tk.OptionMenu(self, set, *options)
+        self.dbx_search_by.grid(row = 1, column = 0)
+        
+        self.lbl_search_for = tk.Label(self, text = "Search For:", font = TITLE_FONT)
         self.lbl_search_for.grid(row = 3, column = 0)
-        self.ent_for = tk.Entry(font = BUTTON_FONT)
+        self.ent_for = tk.Entry(self, font = BUTTON_FONT)
         self.ent_for.grid(row = 4, column = 0)
         
         self.lbl_filters = tk.Label(self, text = "Filters", font = TITLE_FONT)
         self.lbl_filters.grid(row = 1, column = 2)
-        self.lbl_filters = SubFrame(self)
+        self.frm_filters = SubFrame(self)
+        self.frm_filters.grid(row = 2, column = 2)
 
 class Save_Menu(tk.Frame):
     def __init__(self):
@@ -61,13 +71,24 @@ class Save_Menu(tk.Frame):
         self.lbl_title = tk.Label(text = "Save Notice", font = TITLE_FONT)
         self.lbl_title.grid(row = 0, column = 0, sticky = "news")
         self.btn_save = tk.Button(text = "Save", font = BUTTON_FONT)
-        self.btn_save.grid(row = 1, column = 0)        
+        self.btn_save.grid(row = 1, column = 0)
 
 class Edit_Choice_Menu(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
+        
+        options = ['0', '1']
+        
         self.lbl_title = tk.Label(text = "Which file to edit?", font = TITLE_FONT)
         self.lbl_title.grid(row = 0, column = 0, sticky = "news")
+        
+        self.lbl_edit_file = tk.Label(self, text = "File To Edit:", font = TITLE_FONT)
+        self.lbl_edit_file.grid(row = 1, column = 0)
+        self.tkvar = tk.StringVar(self)
+        self.tkvar.set(options)
+        self.dbx_edit_file = tk.OptionMenu(self, set, *options)
+        self.dbx_edit_file.grid(row = 1, column = 0)        
+        
         self.btn_cancel = tk.Button(text = "Cancel", font = BUTTON_FONT)
         self.btn_cancel.grid(row = 1, column = 0)
         self.btn_ok = tk.Button(text = "OK", font = BUTTON_FONT)
@@ -113,17 +134,28 @@ class Edit_Menu(tk.Frame):
         self.lbl_game_title.grid(row = 3, column = 0)        
         
         self.ent_game_title = tk.Entry(font = BUTTON_FONT)
-        self.ent_game_title.grid(row = 3, column = 1)        
+        self.ent_game_title.grid(row = 3, column = 1)
         
 class Remove_Choice_Menu(tk.Frame):
     def __init__(self):
         tk.Frame.__init__(self)
+        
+        options = ['0', '1']
+        
         self.lbl_title = tk.Label(text = "Choose file to remove!", font = TITLE_FONT)
         self.lbl_title.grid(row = 0, column = 0, sticky = "news")
+        
+        self.lbl_remove_file = tk.Label(self, text = "File To Remove:", font = TITLE_FONT)
+        self.lbl_remove_file.grid(row = 1, column = 0)
+        self.tkvar = tk.StringVar(self)
+        self.tkvar.set(options)
+        self.dbx_remove_file = tk.OptionMenu(self, set, *options)
+        self.dbx_remove_file.grid(row = 1, column = 0)        
+        
         self.btn_cancel = tk.Button(text = "Cancel", font = BUTTON_FONT)
         self.btn_cancel.grid(row = 1, column = 0)
         self.btn_remove = tk.Button(text = "Remove", font = BUTTON_FONT)
-        self.btn_remove.grid(row = 1, column = 1)        
+        self.btn_remove.grid(row = 1, column = 1)
 
 class Remove_Menu(tk.Frame):
     def __init__(self):
@@ -133,42 +165,42 @@ class Remove_Menu(tk.Frame):
         self.btn_back = tk.Button(text = "Back", font = BUTTON_FONT)
         self.btn_back.grid(row = 1, column = 0)
         self.btn_confirm = tk.Button(text = "Confirm", font = BUTTON_FONT)
-        self.btn_confirm.grid(row = 1, column = 1)        
+        self.btn_confirm.grid(row = 1, column = 1)
 
 class SubFrame(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, master = parent)
         
-        self.btn_single = tk.Checkbutton(self, text = "title")
-        self.btn_single.grid(row = 0, column = 0)
+        self.chk_title = tk.Checkbutton(self, text = "Title")
+        self.chk_title.grid(row = 0, column = 0)
         
-        self.btn_multi = tk.Checkbutton(self, text = "genre")
-        self.btn_multi.grid(row = 0, column = 1)
+        self.chk_genre = tk.Checkbutton(self, text = "Genre")
+        self.chk_genre.grid(row = 0, column = 1)
         
-        self.btn_played = tk.Checkbutton(self, text = "Developer")
-        self.btn_played.grid(row = 0 , column = 2)
+        self.chk_developer = tk.Checkbutton(self, text = "Developer")
+        self.chk_developer.grid(row = 0 , column = 2)
         
-        self.btn_no_played = tk.Checkbutton(self, text = "Publisher")
-        self.btn_no_played.grid(row = 1, column = 0)
+        self.chk_publisher = tk.Checkbutton(self, text = "Publisher")
+        self.chk_publisher.grid(row = 1, column = 0)
         
-        self.btn_purchased = tk.Checkbutton(self, text = "Year")
-        self.btn_purchased.grid(row = 1, column = 1)
+        self.chk_year = tk.Checkbutton(self, text = "Year")
+        self.chk_year.grid(row = 1, column = 1)
         
-        self.btn_no_purchased = tk.Checkbutton(self, text = "Price")
-        self.btn_no_purchased.grid(row = 1, column = 2)
+        self.chk_price = tk.Checkbutton(self, text = "Price")
+        self.chk_price.grid(row = 1, column = 2)
         
-        self.btn_console = tk.Checkbutton(self, text = "Console")
-        self.btn_console.grid(row = 2, column = 0)
+        self.chk_console = tk.Checkbutton(self, text = "Console")
+        self.chk_console.grid(row = 2, column = 0)
         
-        self.btn_gamemode = tk.Checkbutton(self, text = "Gamemode")
-        self.btn_gamemode.grid(row = 2, column = 1)
+        self.chk_gamemode = tk.Checkbutton(self, text = "Gamemode")
+        self.chk_gamemode.grid(row = 2, column = 1)
         
-        self.btn_notes = tk.Checkbutton(self, text = "Notes")
-        self.btn_notes.grid(row = 2, column = 2)
+        self.chk_notes = tk.Checkbutton(self, text = "Notes")
+        self.chk_notes.grid(row = 2, column = 2)
         
-        self.grid_rowconfigure(0, weight = 1)
-        self.grid_rowconfigure(1, weight = 1)
-        self.grid_rowconfigure(2, weight = 1)
+        #self.grid_rowconfigure(0, weight = 1)
+        #self.grid_rowconfigure(1, weight = 1)
+        #self.grid_rowconfigure(2, weight = 1)
         
         self.grid_columnconfigure(0, weight = 1)
         self.grid_columnconfigure(1, weight = 1)
@@ -182,27 +214,28 @@ if __name__ == "__main__":
     pickle_file = open("game_lib.pickle", "rb")
     root = tk.Tk()
     root.title("Game Library")
-    root.geometry("500x500")
+    root.geometry("750x750")
     
-    #main_menu = MainMenu()
-    #main_menu.grid(row = 0, column = 0)
+    main_menu = MainMenu()
+    main_menu.grid(row = 0, column = 0, sticky = "news")
     
     search_menu = Search_Menu()
-    search_menu.grid(row = 0, column = 0)
+    search_menu.grid(row = 0, column = 0, sticky = "news")
     
-    #save_menu = Save_Menu()
-    #save_menu.grid(row = 0, column = 0)
+    save_menu = Save_Menu()
+    save_menu.grid(row = 0, column = 0, sticky = "news")
     
-    #edit_choice_menu = Edit_Choice_Menu()
-    #edit_choice_menu.grid(row = 0, column = 0)
+    edit_choice_menu = Edit_Choice_Menu()
+    edit_choice_menu.grid(row = 0, column = 0, sticky = "news")
     
-    #edit_menu = Edit_Menu()
-    #edit_menu.grid(row = 0, column = 0)
+    edit_menu = Edit_Menu()
+    edit_menu.grid(row = 0, column = 0, sticky = "news")
     
-    #remove_choice_menu = Remove_Choice_Menu()
-    #remove_choice_menu.grid(row = 0, column = 0)
+    remove_choice_menu = Remove_Choice_Menu()
+    remove_choice_menu.grid(row = 0, column = 0, sticky = "news")
     
-    #remove_menu = Remove_Menu()
-    #remove_menu.grid(row = 0, column = 0)
+    remove_menu = Remove_Menu()
+    remove_menu.grid(row = 0, column = 0, sticky = "news")
     
+    main_menu.tkraise()
     root.mainloop()
